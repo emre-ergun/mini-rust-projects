352824c3c7124b37ba33fc12eea26290197cc86e
Emre Ergun <emreergun@engramsoft.com>
2024-02-17 19:43:43 +0300

    progress bar example for cli added

---
+++ cli_exercises/cli_progressbar/src/main.rs
@@ -0,0 +1,15 @@
+use indicatif;
+
+fn main() {
+    let pb = indicatif::ProgressBar::new(100);
+    for _i in 0..100 {
+        do_hard_work();
+        //pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
+}
+
+fn do_hard_work() {
+    std::thread::sleep(std::time::Duration::from_millis(50));
+}
