deb99af46894cff3737e9c49845d5ef5cffddd45
Emre Ergun <emreergun@engramsoft.com>
2024-02-19 16:46:49 +0300

    frontend part completed - backend updated for the cors layer

--- web_development/backend/src/main.rs
+++ web_development/backend/src/main.rs
@@ -1,15 +1,24 @@
-use axum::{routing::get, Json, Router};
+use axum::{
+    http::{header::CONTENT_TYPE, HeaderValue, Method},
+    routing::get,
+    Json, Router,
+};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(handler))
-        .route("/counter", get(counter_handler));
+    let app = Router::new().route("/", get(handler)).layer(
+        tower_http::cors::CorsLayer::new()
+            .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+            .allow_headers([CONTENT_TYPE])
+            .allow_methods([Method::GET]),
+    );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
     println!("Server will be started on: {addr}");
-    let listener = tokio::net::TcpListener::bind(addr).await.expect("Could not bind!");
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .expect("Could not bind!");
     axum::serve(listener, app).await.expect("Could not started");
 }
 
@@ -21,18 +30,5 @@ struct Message {
 async fn handler() -> Json<Message> {
     Json(Message {
         message: String::from("Hello World!"),
-    })
-}
-
-#[derive(serde::Serialize)]
-struct Counter {
-    counter: u32,
-    message: String,
-}
-
-async fn counter_handler() -> Json<Counter> {
-    Json(Counter {
-        counter: 42,
-        message: String::from("Answer"),
     })
 }
---
+++ web_development/frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { ref } from 'vue';
+const message = ref(null);
+
+async function getMessage() {
+  const response = await fetch('http://localhost:3030');
+  const json = await response.json();
+  message.value = json.message;
+}
+</script>
+
+<template>
+  <h1>Rust + Vue</h1>
+  <div v-if="message">
+    Received: {{ message }}
+  </div>
+  <div v-else>
+    No Message
+  </div>
+  <button @click="getMessage()">Get Message</button>
+</template>
