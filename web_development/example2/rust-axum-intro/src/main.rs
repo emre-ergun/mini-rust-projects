b0a349d310d0478cd395d231c733456ec476604a
Emre Ergun <emreergun@engramsoft.com>
2024-02-20 18:06:03 +0300

    routers added to project

--- web_development/example2/rust-axum-intro/src/main.rs
+++ web_development/example2/rust-axum-intro/src/main.rs
@@ -2,12 +2,16 @@
 
 use std::net::SocketAddr;
 
+use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse};
 use axum::routing::get;
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let route_hello = axum::Router::new().route("/hello", get(handler_hello));
+    let route_hello = axum::Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Server will be listening on {addr}");
@@ -19,7 +23,20 @@ async fn main() {
         .expect("Could not started");
 }
 
-async fn handler_hello() -> impl IntoResponse {
-    println!("->> {:<12} - handler_hello", "HANDLER");
-    Html("Hello <strong>World!</strong>")
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+    surname: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("Hello <strong>{name}!</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {name:?}", "HANDLER");
+    Html(format!("Hello <strong>{name}!</strong>"))
 }
--- web_development/example2/rust-axum-intro/tests/quick_dev.rs
+++ web_development/example2/rust-axum-intro/tests/quick_dev.rs
@@ -6,7 +6,8 @@ use anyhow::{Ok, Result};
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Emre").await?.print().await?;
+    hc.do_get("/hello2/Emre").await?.print().await?;
 
     Ok(())
 }
