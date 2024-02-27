f8541d256071f0ec294b2739858675a2da519fa3
Emre Ergun <emreergun@engramsoft.com>
2024-02-27 19:15:38 +0300

    refcell smart pointer example added into rc smart pointer example

--- bootcamp/rc_smart_pointer/src/main.rs
+++ bootcamp/rc_smart_pointer/src/main.rs
@@ -1,20 +1,34 @@
-use std::{clone, rc::Rc};
-struct Database {}
+use std::rc::Rc;
+use std::cell::RefCell;
+
+struct Database {
+    max_connections: u32,
+}
 
 struct AuthService {
-    db: Rc<Database>,
+    db: Rc<RefCell<Database>>,
 }
 
 struct ContentService {
-    db: Rc<Database>,
+    db: Rc<RefCell<Database>>,
 }
 fn main() {
     // the Rc smart pointer is similar to shared_pointer in C++
-    let db = Rc::new(Database {});
+    let db = Rc::new(RefCell::new(Database {
+        max_connections: 100,
+    }));
     // the clone function in the Rc module is not actually cloning the the database
     // it will simply increment the reference counter of the smart pointer
     let auth_service = AuthService { db: Rc::clone(&db) };
     println!("{}", Rc::strong_count(&db));
     let content_service = ContentService { db: Rc::clone(&db) };
     println!("{}", Rc::strong_count(&db));
+    //Rc smart pointer only allows immutable shared ownership of a value.
+    //db.max_connections = 200;
+    // after RefCell, it works
+    //db.borrow_mut().max_connections = 200;
+    let mut r1 = db.borrow_mut();
+    let r2 = db.borrow_mut();
+    r1.max_connections = 200;
+
 }
