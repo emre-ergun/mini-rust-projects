dad46412e39aa834e5a2f1efbba9a8c153218984
Emre Ergun <emreergun@engramsoft.com>
2024-02-28 18:26:49 +0300

    some fundamental methods added

--- cli_exercises/cli_with_clap/notes.md
+++ cli_exercises/cli_with_clap/notes.md
@@ -12,7 +12,23 @@
 
 1. arg()
    1. It is used to create an argument.
-2. get_matches()
+2. subcommand()
+   1. It is used to make different command for different arguments and group them together
+   2. If the global argument is defined which is not included in any subcommand should be called before the command when it is used. (fluffy in the code is global argument)
+3. about()
+   1. Used to add help text to explain what the application does to show up along with help menu
+4. get_matches()
    1. It invokes the parser.
-3. short() and long() methods
+5. short() and long() methods
    1. they are used to convert positional arguments into named arguments by specifiying long string or short character for the argument.
+6. alias() or aliases() methods
+   1. They are used to be alternative for the named arguments.
+   2. They dont't show up along with the help menu
+   3. They help different user for different usage
+   4. If a named argument is --lname, a user can use it --lastname or --last-name If they are defined as aliases
+7. required()
+   1. If an argument has to be used we can specify it by using required method with boolean true.
+8. help()
+   1. It is used to add description for the argument to show up along with help menu
+9.  conflicts_with()
+   1. We can add one of other arguments as a conflicted argument which can not be used together anymore
--- cli_exercises/cli_with_clap/src/main.rs
+++ cli_exercises/cli_with_clap/src/main.rs
@@ -1,13 +1,41 @@
-use clap::{command, Arg};
+use clap::{command, Arg, ArgGroup, Command};
 fn main() {
     let match_result = command!()
-    .arg(
-        Arg::new("firstname").short('f').long("fname")
-    )
-    .arg(
-        Arg::new("lastname").short('l').long("lname")
-    )
-    .version("v0.1.0")
-    .get_matches();
-
+        .subcommand(
+            Command::new("register-person")
+                .arg(
+                    Arg::new("firstname")
+                        .short('f')
+                        .long("first-name")
+                        .aliases(["fname", "firstname"])
+                        .required(true)
+                        .help("Take the person's first name"),
+                )
+                .arg(
+                    Arg::new("lastname")
+                        .short('l')
+                        .long("last-name")
+                        .aliases(["lname", "lastname"])
+                        .required(true)
+                        .help("Take the person's last name"), //.conflicts_with("firstname"),
+                ),
+        )
+        .subcommand(
+            Command::new("register-pet").arg(
+                Arg::new("petname")
+                    .short('p')
+                    .long("petname")
+                    .aliases(["pet-name", "pname"])
+                    .required(true)
+                    .help("Take the pet's name"),
+            ),
+        )
+        .about("This application registers people with their doctor's office")
+        .arg(
+            Arg::new("fluffy")
+                .long("fluffy")
+                .help("Specifiy if the person wears a fluffy coat 🥼 or not"),
+        )
+        .version("v0.1.0")
+        .get_matches();
 }
